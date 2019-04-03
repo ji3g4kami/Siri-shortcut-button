d991e9bd534b47444bc4cc357420eeedbe320129
吳登秝 <ji3g4kami@gmail.com>
2019-04-03 11:19:40 +0800

    [add] use UUID to keep tracl of Siri button

--- AudioCast/EpisodeTableViewController.swift
+++ AudioCast/EpisodeTableViewController.swift
@@ -50,31 +50,33 @@ class EpisodeTableViewController: UITableViewController {
         super.viewWillAppear(animated)
         navigationController?.isToolbarHidden = true
     }
-    
+
     func addSiriButton(to view: UIView) {
         
         siriButton.translatesAutoresizingMaskIntoConstraints = false
+
+        
+        INVoiceShortcutCenter.shared.getAllVoiceShortcuts { [unowned self] (allVoiceShortcuts, error) in
+            if let allVoiceShortcuts = allVoiceShortcuts {
+                if let identifier = UserDefaults.standard.string(forKey: self.libraryContainer.title),
+                    let shortcut = allVoiceShortcuts.first(where: { (voiceShortcut) -> Bool in
+                    return voiceShortcut.shortcut.intent?.identifier == identifier
+                })?.shortcut {
+                    self.siriButton.shortcut = shortcut
+                } else {
+                    let intent: INPlayMediaIntent = PlayRequest(container: self.libraryContainer, episodes: self.episodes).intent
+                    intent.suggestedInvocationPhrase = self.libraryContainer.title
+                    self.siriButton.shortcut = INShortcut(intent: intent)!
+                }
+            }
+        }
+        
+        siriButton.delegate = self
         
         view.addSubview(siriButton)
         view.centerXAnchor.constraint(equalTo: siriButton.centerXAnchor).isActive = true
         view.centerYAnchor.constraint(equalTo: siriButton.centerYAnchor).isActive = true
-        
-        siriButton.addTarget(self, action: #selector(addToSiri(_:)), for: .touchUpInside)
-    }
-    
-    @objc
-    func addToSiri(_ sender: Any) {
-        
-        let intent: INPlayMediaIntent = PlayRequest(container: libraryContainer, episodes: episodes).intent
-        
-        intent.suggestedInvocationPhrase = "\(libraryContainer.title)"
-        if let shortcut = INShortcut(intent: intent) {
-            let viewController = INUIAddVoiceShortcutViewController(shortcut: shortcut)
-            viewController.modalPresentationStyle = .formSheet
-            viewController.delegate = self // Object conforming to `INUIAddVoiceShortcutViewControllerDelegate`.
-            siriButton.shortcut = shortcut
-            present(viewController, animated: true, completion: nil)
-        }
+
     }
     
     private func play(episodes: [PodcastEpisode]?) {
@@ -126,23 +128,48 @@ extension EpisodeTableViewController {
     }
 }
 
+extension EpisodeTableViewController: INUIAddVoiceShortcutButtonDelegate {
+    func present(_ addVoiceShortcutViewController: INUIAddVoiceShortcutViewController, for addVoiceShortcutButton: INUIAddVoiceShortcutButton) {
+        addVoiceShortcutViewController.delegate = self
+        addVoiceShortcutViewController.modalPresentationStyle = .formSheet
+        present(addVoiceShortcutViewController, animated: true, completion: nil)
+    }
+    
+    func present(_ editVoiceShortcutViewController: INUIEditVoiceShortcutViewController, for addVoiceShortcutButton: INUIAddVoiceShortcutButton) {
+        editVoiceShortcutViewController.delegate = self
+        editVoiceShortcutViewController.modalPresentationStyle = .formSheet
+        present(editVoiceShortcutViewController, animated: true, completion: nil)
+    }
+}
+
 extension EpisodeTableViewController: INUIAddVoiceShortcutViewControllerDelegate, INUIEditVoiceShortcutViewControllerDelegate {
+    
     func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didUpdate voiceShortcut: INVoiceShortcut?, error: Error?) {
-        controller.dismiss(animated: true, completion: nil)
+        controller.dismiss(animated: true) { [unowned self] in
+            let identifier = voiceShortcut?.shortcut.intent?.identifier
+            UserDefaults.standard.set(identifier, forKey: self.libraryContainer.title)
+            self.addSiriButton(to: self.view)
+        }
     }
-    
+
     func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didDeleteVoiceShortcutWithIdentifier deletedVoiceShortcutIdentifier: UUID) {
-        controller.dismiss(animated: true, completion: nil)
+        controller.dismiss(animated: true) { [unowned self] in
+            UserDefaults.standard.removeObject(forKey: self.libraryContainer.title)
+        }
     }
-    
+
     func editVoiceShortcutViewControllerDidCancel(_ controller: INUIEditVoiceShortcutViewController) {
         controller.dismiss(animated: true, completion: nil)
     }
-    
+
     func addVoiceShortcutViewController(_ controller: INUIAddVoiceShortcutViewController, didFinishWith voiceShortcut: INVoiceShortcut?, error: Error?) {
-        controller.dismiss(animated: true, completion: nil)
+        controller.dismiss(animated: true) { [unowned self] in
+            let identifier = voiceShortcut?.shortcut.intent?.identifier
+            UserDefaults.standard.set(identifier, forKey: self.libraryContainer.title)
+            self.addSiriButton(to: self.view)
+        }
     }
-    
+
     func addVoiceShortcutViewControllerDidCancel(_ controller: INUIAddVoiceShortcutViewController) {
         controller.dismiss(animated: true, completion: nil)
     }
